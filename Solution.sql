4cc391d0a9fc8b12aefd1e5c2b124f936cfa8074
Alper Kaya <73350743+alperkaya0@users.noreply.github.com>
2022-01-20 06:58:53 +0300

    Create Solution.sql

---
+++ Solution.sql
@@ -0,0 +1,26 @@
+SELECT AVG(rental_rate) FROM film;
+SELECT COUNT(*) FROM film WHERE title LIKE 'C%';
+SELECT MAX(length) FROM film WHERE rental_rate = 0.99;
+SELECT COUNT(DISTINCT replacement_cost) FROM film WHERE length > 150;
+
+ENGLISH:
+
+--Hello,
+
+--Code the given query scenarios by using the example database dvdrental.
+
+--What is the average of the rental_rate column values that reside at the film data table ?
+--How many rows of the film data table starts with character 'C' ?
+--What is the length of the longest film that has rental_rate column value equals to 0.99 ?
+--How many distinct replacement_cost values are there for the films that have length column value bigger than 150 ?
+
+TURKISH:
+
+--Merhabalar,
+
+--Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+--film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+--film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+--film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+--film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
